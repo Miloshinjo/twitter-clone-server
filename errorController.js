43c916b8b1bb1527586896a7d5d4ec89f62d0738
Milos <miloshdz@gmail.com>
2020-08-16 23:24:25 +0200

    Fix error controller

--- errorController.js
+++ errorController.js
@@ -1,3 +1,41 @@
+const express = require('express');
+const AppError = require('./appError');
+
+// user posted wrong format of data
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+// duplicate fields (mongodb error)
+function handleDuplicateFieldsDB(err) {
+  const value = err.message.split('"')[1];
+
+  let message = `Duplicate field value: ${value}. Please use another value.`;
+
+  if (err.keyValue.email) {
+    message = 'User already registered';
+  }
+
+  return new AppError(message, 400);
+}
+
+// validation error
+function handleValidationErrorDB(err) {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+}
+
+function handleJWTError() {
+  return new AppError('Invalid token. Please log in again.', 401);
+}
+
+function handleJWTExpiredError() {
+  return new AppError('Your token has expired. Please log in again.', 401);
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -38,6 +76,13 @@ module.exports = (err, req, res) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     error.message = err.message;
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
